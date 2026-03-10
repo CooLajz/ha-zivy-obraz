40d3d692a2732ef28770616e1d2567a3ffbb84a1
CooLajz <coolajz@coolajz.cz>
2026-03-10 12:16:15 +0100

    Update const.py

--- custom_components/zivy_obraz/const.py
+++ custom_components/zivy_obraz/const.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 DOMAIN = "zivy_obraz"
 
 CONF_EXPORT_KEY = "export_key"
+CONF_USE_GROUP_FILTER = "use_group_filter"
+CONF_GROUP_ID = "group_id"
 CONF_SCAN_INTERVAL = "scan_interval"
 CONF_TIMEOUT = "timeout"
 CONF_OVERDUE_TOLERANCE = "overdue_tolerance"
@@ -13,8 +15,8 @@ CONF_LABEL = "label"
 CONF_PREFIX = "prefix"
 CONF_PUSH_INTERVAL = "push_interval"
 
-DEFAULT_SCAN_INTERVAL = 600
-DEFAULT_TIMEOUT = 30
+DEFAULT_SCAN_INTERVAL = 300
+DEFAULT_TIMEOUT = 15
 DEFAULT_OVERDUE_TOLERANCE = 30
 
 DEFAULT_PUSH_ENABLED = False
@@ -22,8 +24,9 @@ DEFAULT_IMPORT_KEY = ""
 DEFAULT_LABEL = "ZivyObraz"
 DEFAULT_PREFIX = ""
 DEFAULT_PUSH_INTERVAL = 300
+DEFAULT_USE_GROUP_FILTER = False
 
-ZIVY_OBRAZ_EXPORT_URL = "http://out.zivyobraz.eu/"
+ZIVY_OBRAZ_EXPORT_URL = "https://out.zivyobraz.eu/"
 ZIVY_OBRAZ_PUSH_URL = "https://in.zivyobraz.eu/"
 
 MAX_PUSH_URL_LENGTH = 1800
