26ef726d5a81907bd56f59bd3a144886fde24940
CooLajz <coolajz@coolajz.cz>
2026-03-10 08:18:57 +0100

    Update const.py

--- custom_components/zivy_obraz/const.py
+++ custom_components/zivy_obraz/const.py
@@ -2,13 +2,30 @@ from __future__ import annotations
 
 DOMAIN = "zivy_obraz"
 
-CONF_URL = "url"
+CONF_EXPORT_KEY = "export_key"
 CONF_SCAN_INTERVAL = "scan_interval"
 CONF_TIMEOUT = "timeout"
 CONF_OVERDUE_TOLERANCE = "overdue_tolerance"
 
-DEFAULT_SCAN_INTERVAL = 300
-DEFAULT_TIMEOUT = 15
+CONF_PUSH_ENABLED = "push_enabled"
+CONF_IMPORT_KEY = "import_key"
+CONF_LABEL = "label"
+CONF_PREFIX = "prefix"
+CONF_PUSH_INTERVAL = "push_interval"
+
+DEFAULT_SCAN_INTERVAL = 600
+DEFAULT_TIMEOUT = 30
 DEFAULT_OVERDUE_TOLERANCE = 30
+
+DEFAULT_PUSH_ENABLED = False
+DEFAULT_IMPORT_KEY = ""
+DEFAULT_LABEL = "ZivyObraz"
+DEFAULT_PREFIX = ""
+DEFAULT_PUSH_INTERVAL = 300
+
+ZIVY_OBRAZ_EXPORT_URL = "http://out.zivyobraz.eu/"
+ZIVY_OBRAZ_PUSH_URL = "https://in.zivyobraz.eu/"
+
+MAX_PUSH_URL_LENGTH = 1800
 
 PLATFORMS = ["sensor", "binary_sensor"]
